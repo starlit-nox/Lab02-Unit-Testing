3e84010ef4016358e6fe50f321607ac4ca177585
starlit-nox <kdkelly.1998@gmail.com>
2023-07-12 07:53:46 -0500

    Test Not Working
    - Added test, might be in wrong folder???

--- Lab02-UnitTesting-Test/UnitTest1.cs
+++ Lab02-UnitTesting-Test/UnitTest1.cs
@@ -1,10 +1,30 @@
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+
 namespace Lab02_UnitTesting_Test
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void ViewBalanceTest()
         {
+            Program.Balance = 100;
+            decimal result = Program.ViewBalance();
+            Assert.Equal(100, result);
+        }
+        [Fact]
+        public void WithdrawTest()
+        {
+            Program.Balance = 100;
+            decimal result = Program.Withdraw(60);
+            Assert.Equal(40, result);
+        }
+        [Fact]
+        public void DepositTest()
+        {
+            Program.Balance = 100;
+            decimal result = Program.Deposit(60);
+            Assert.Equal(160, result);
+
 
         }
     }
